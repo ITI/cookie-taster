e7f4fe2f5adc8b41c81290415f8a03f7473b13c3
zanema <amenaz1@gmail.com>
2019-02-22 16:05:50 -0600

    basic downloading version

--- background.js
+++ background.js
@@ -1,5 +1,32 @@
 var script_cookie_hashes = {};
 
+function onStartedDownload(id) {
+    console.log(`Started downloading: ${id}`);
+}
+
+function onFailed(error) {
+    console.log(`Download failed: ${error}`);
+}
+
+function sanitizeFilename(name) {
+    return name.replace(/[^a-z0-9\.]/gi, '_').toLowerCase();
+}
+
+function download(data, fname) {
+    var blob = new Blob([data], {type: 'application/javascript'})
+    var blob_url = URL.createObjectURL(blob);
+
+    var downloading = browser.downloads.download({
+          url : blob_url,
+          filename : sanitizeFilename(fname),
+          conflictAction : 'uniquify',
+    });
+
+    //console.log(sanitizeFilename(fname));
+
+    downloading.then(onStartedDownload, onFailed);
+}
+
 function removeHeader(headers, name) {
   for (var i = 0; i < headers.length; i++) {
     if (headers[i].name.toLowerCase() == name) {
@@ -30,18 +57,16 @@ function listener(details) {
 
   filter.ondata = event => {
     let str = decoder.decode(event.data, {stream: true});
-    var hash = sha256.create();
-    hash.update(str);
-      
     if (!(details.url in script_cookie_hashes)){
-      script_cookie_hashes[details.url] = hash.hex();
+      script_cookie_hashes[details.url] = str;
       // trigger second download of the same script
       
       browser.tabs.sendMessage(details.tabId, {'url': details.url});
     } else if (details.url in script_cookie_hashes) {
       // check for differences and write to console
-      if (hash.hex() != script_cookie_hashes[details.url]) {
-        console.log("CHANGE: " + details.url);
+      if (str != script_cookie_hashes[details.url]) {
+        download(str, details.url + ".no_cookie")
+        download(script_cookie_hashes[details.url], details.url + ".cookie")
       }
     } 
     filter.write(encoder.encode(str));
