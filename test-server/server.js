9e02c20ee6fee1f168cbdf437ab2c402e01f1dad
zanema <amenaz1@gmail.com>
2019-07-08 11:04:12 -0500

    add request header logging to test server

--- test-server/server.js
+++ test-server/server.js
@@ -1,23 +1,33 @@
 var express = require('express'), http = require('http'), cookieParser = require('cookie-parser'), morgan = require('morgan');
 var app = express();
 
-app.use(morgan('tiny'));
+app.use(morgan('combined'));
+app.use(function (req, res, next) {
+    console.log(req.headers);
+    next();
+});
 
 // need cookieParser middleware before we can do anything with cookies
 app.use(cookieParser());
 
 // set a cookie
 app.use(function (req, res, next) {
+
+  if (req.path != "/test.json") {
+    next();
+  }
+
   // check if client sent cookie
   var cookie = req.cookies.cookieName;
-  if (cookie === undefined)
-  {
+  if (cookie === undefined) {
     // no: set a new cookie
     var randomNumber=Math.random().toString();
     randomNumber=randomNumber.substring(2,randomNumber.length);
     res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
-  } 
-  next(); // <-- important!
+    res.json({'data': 'unauthenticated'});
+  } else {
+    res.json({'data': 'cookie authenticated'}); 
+  }
 });
 
 // let static middleware do its job
